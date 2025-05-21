68b053bc5c361f29ec54e53c5b89629b286234f4
shijibijo84 <shijib.bijo@gmail.com>
2025-05-22 00:41:57 +0200

    #8-data rendered

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -1,15 +1,23 @@
 import { ShoppingCart } from "lucide-react"
 
-const Card = () => {
+type dataProps = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imgSrc: string;
+}
+
+const Card = ({ data }: { data: dataProps }) => {
     return (
         <div className="p-4">
             <div className="w-60 rounded-2xl overflow-hidden shadow-md bg-white p-4 text-center ">
-                <img src="https://www.themealdb.com/images/ingredients/lime-small.png" alt="product" />
+                <img src={data.imgSrc} alt="product" />
                 <div className="mt-4">
-                    <h2 className="text-lg text-gray-800 font-semibold">Product Name</h2>
-                    <p className="text-gray-600 mt-1">Short Description</p>
+                    <h2 className="text-lg text-gray-800 font-semibold">{data.name}</h2>
+                    <p className="text-gray-600 mt-1">{data.description}</p>
                     <div className="flex justify-between items-center mt-4">
-                        <span className="text-green-600 font-bold">200kr</span>
+                        <span className="text-green-600 font-bold">{data.price}kr</span>
                         <ShoppingCart
                             size={24}
                             className="text-blue-500 cursor-pointer hover:text-blue-600"
