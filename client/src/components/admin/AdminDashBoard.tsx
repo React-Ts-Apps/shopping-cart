999756c6ea1cf1b869583f12e0f176548ec4e4bf
shijibijo84 <shijib.bijo@gmail.com>
2025-05-22 12:43:52 +0200

    #10-add admin page items

--- client/src/components/admin/AdminDashBoard.tsx
+++ client/src/components/admin/AdminDashBoard.tsx
@@ -1,10 +1,13 @@
 
+import ProductForm from "./ProductForm"
 import ProductList from "./ProductList"
 
 const AdminDashBoard = () => {
 
-    return (<div>
-        <ProductList />
-    </div>)
+    return (
+        <div className="flex">
+            <div className="w-1/3"> <ProductForm /></div>
+            <div className="w-2/3"> <ProductList /></div>
+        </div>)
 }
 export default AdminDashBoard
--- client/src/components/admin/ProductList.tsx
+++ client/src/components/admin/ProductList.tsx
@@ -8,26 +8,29 @@ const ProductList = () => {
         instance.get('/admin/items').then((res) => setData(res.data)).catch((error) => console.error(error))
     }, [])
     return (
-        <div>
-            <h2 className=" p-5 pl-10 font-semibold">Product List</h2>
+        <div className="space-y-4 max-w-3xl p-10  mx-auto">
+            <h2 className="font-semibold">Added Items({data.length})</h2>
             <div className="overflow-x-auto">
                 <table className="border border-gray-200 rounded-lg shadow-sm">
                     <thead className="bg-gray-100">
                         <tr>
+                            <th className="py-2 px-4 text-left border-b">ItemNo</th>
                             <th className="py-2 px-4 text-left border-b">Name</th>
                             <th className="py-2 px-4 text-left border-b">Description</th>
                             <th className="py-2 px-4 text-left border-b">Price</th>
-                            <th className="py-2 px-4 text-left border-b">Image Url</th>
+                            <th className="py-2 px-4 text-left border-b">Image</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             data.map((item) => (
                                 <tr key={item.id}>
+                                    <td className="py-2 px-4 text-left border-b">{item.id}</td>
                                     <td className="py-2 px-4 text-left border-b">{item.name}</td>
                                     <td className="py-2 px-4 text-left border-b">{item.description}</td>
                                     <td className="py-2 px-4 text-left border-b">{item.price}kr</td>
-                                    <td className="py-2 px-4 text-left border-b">{item.imgSrc}</td>
+                                    <td className="py-2 px-4 text-left border-b">
+                                        <img src={item.imgSrc} alt={item.name} className="w-16 h-16" /></td>
                                 </tr>
                             ))
                         }
