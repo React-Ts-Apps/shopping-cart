7cc9ae7da4b73b934a6d2cde791202511589d547
shijibijo84 <shijib.bijo@gmail.com>
2025-05-26 16:30:07 +0200

    #18-layout changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,13 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import ItemList from "./components/user/ItemList"
 import AdminDashBoard from "./components/admin/AdminDashBoard"
 import ActionPage from "./components/user/ActionPage"
-import Layout from "./components/ui/LayOut"
+import LayOut from "./components/ui/LayOut"
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />} >
+        <Route path='/' element={<LayOut />} >
           <Route index element={<ItemList />} />
           <Route path='/home' element={<ItemList />} />
           <Route path='/:action' element={<ActionPage />} />
