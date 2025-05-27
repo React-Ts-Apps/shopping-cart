ec95018e226bc1c2fdeb3f5ea6168def5bbc345e
shijibijo84 <shijib.bijo@gmail.com>
2025-05-27 11:13:49 +0200

    #18-fix test

--- server/tests/items.test.js
+++ server/tests/items.test.js
@@ -4,7 +4,7 @@ const app = require('../app')
 
 describe('GET /items', () => {
     it('Should return all items with status 200', async () => {
-        const response = await request(app).get('/items')
+        const response = await request(app).get('/home/items')
         expect(response.statusCode).toBe(200)
         expect(Array.isArray(response.body)).toBe(true)
     })
