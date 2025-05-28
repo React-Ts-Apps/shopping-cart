f6a405d8be9f0bb63d2d3af6ae31ce42c561647d
shijibijo84 <shijib.bijo@gmail.com>
2025-05-28 10:00:31 +0200

    #28-added cart operations

--- client/src/redux/slices/cartSlice.ts
+++ client/src/redux/slices/cartSlice.ts
@@ -15,8 +15,27 @@ const cartSlice = createSlice({
             } else {
                 state.items.push({ ...item, quantity: 1 })
             }
+        },
+        incrementCartItem: (state, action) => {
+            const item = state.items.find(i => i._id === action.payload)
+            if (item) item.quantity += 1
+        },
+        decrementCartItem: (state, action) => {
+            const item = state.items.find(i => i._id === action.payload)
+            if (item) item.quantity -= 1
+        },
+        deleteCartItem: (state, action) => {
+            state.items = state.items.filter(i => i._id !== action.payload)
+        },
+        clearCart: (state) => {
+            state.items = []
         }
+
     }
 })
-export const { addToCart } = cartSlice.actions
+export const { addToCart,
+    incrementCartItem,
+    decrementCartItem,
+    deleteCartItem,
+    clearCart } = cartSlice.actions
 export default cartSlice.reducer
