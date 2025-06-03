7b129abb127836fa4f3c1f87210b24c24c51d746
shijibijo84 <shijib.bijo@gmail.com>
2025-06-04 01:09:19 +0200

    #31-configure remote path

--- micro-frontends/shopping-host/vite.config.ts
+++ micro-frontends/shopping-host/vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import federation from '@originjs/vite-plugin-federation';
 
+import dotenv from 'dotenv'
+dotenv.config()
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +13,7 @@ export default defineConfig({
     name: 'shopping-host',
     filename: 'remoteEntry.js',
     remotes: {
-      'recipes-remote': 'http://localhost:5001/assets/remoteEntry.js',
+      'recipes-remote': `${process.env.VITE_REMOTE_RECIPES}/assets/remoteEntry.js`,
     },
     shared: ['react', 'react-dom', 'react-router-dom'],
   }),
