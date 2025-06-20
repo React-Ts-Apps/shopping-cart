fc76b3f8c796add81e38e442e3675b784275af64
shijibijo84 <shijib.bijo@gmail.com>
2025-06-20 07:13:24 +0200

    #50-admin routes for users

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,9 +7,13 @@ import {
     resetPassword,
     getUserProfile,
     changePassword,
-    updateProfile
+    updateProfile,
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser
 } from '../controllers/authController.js'
-import { isAuthenticated } from '../middlewares/authenticate.js'
+import { isAuthenticated, isAuthenticatedRole } from '../middlewares/authenticate.js'
 
 const router = Router()
 
@@ -22,5 +26,11 @@ router.route('/password/change').put(isAuthenticated, changePassword)
 router.route('/myprofile').get(isAuthenticated, getUserProfile)
 router.route('/update/profile').put(isAuthenticated, updateProfile)
 
+//Admin routes
+router.route('/admin/users').get(isAuthenticated, isAuthenticatedRole('admin'), getUsers)
+router.route('/admin/user/:id')
+    .get(isAuthenticated, isAuthenticatedRole('admin'), getUser)
+    .put(isAuthenticated, isAuthenticatedRole('admin'), updateUser)
+    .delete(isAuthenticated, isAuthenticatedRole('admin'), deleteUser)
 
 export default router
