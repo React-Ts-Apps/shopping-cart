844e31556e3c5ddb44d086bb83d09bdb3c0fbc26
shijibijo84 <shijib.bijo@gmail.com>
2025-07-15 22:18:35 +0200

    #92-fix cookies

--- server/utils/jwt.js
+++ server/utils/jwt.js
@@ -1,10 +1,13 @@
 const sendJwt = (user, res, statusCode) => {
     const token = user.getJwtToken()
 
+    const isProd = process.env.NODE_ENV === 'production'
     //Setting cookies
     const options = {
         expires: new Date(Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: isProd,
+        sameSite: isProd ? 'None' : 'Lax'
     }
 
     res.status(statusCode)
